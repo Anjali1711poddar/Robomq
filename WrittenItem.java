8f74299f44deb3e8fde0dd4ad0dbb0f502c4624f
Anjali1711poddar <60092976+Anjali1711poddar@users.noreply.github.com>
2020-01-21 18:29:46 +0530

    Add files via upload

---
+++ WrittenItem.java
@@ -0,0 +1,19 @@
+
+public abstract class WrittenItem extends Item {
+	private String author;
+
+	public WrittenItem() {
+
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	
+
+}
